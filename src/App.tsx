948b0aa5f6b45ca8dc639b00bcb2827ecb5197c5
binoyam <binyam.techan01@gmail.com>
2024-01-02 01:34:53 +0300

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./models/models";
 import TodoList from "./components/TodoList";
+import { DragDropContext } from "react-beautiful-dnd";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Array<Todo>>([]);
-  // const [CompletedTodos, setCompletedTodos] = useState<Array<Todo>>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
   // console.log(todo);
 
   const handleAdd = (e: React.FormEvent) => {
@@ -20,14 +21,20 @@ const App: React.FC = () => {
   // console.log(todos);
 
   return (
-    <div className="App">
-      <span className="heading">TAskify</span>
-      
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    <DragDropContext>
+      <div className="App">
+        <span className="heading">TAskify</span>
 
-      <TodoList todos={todos} setTodos={setTodos}/>
-     
-    </div>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,6 +6,8 @@ import SingleTodo from "./SingleTodo";
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  completedTodos: Todo[];
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
@@ -22,7 +24,7 @@ const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
           />
         ))}
       </div>
-        <div className="remove todos">
+      <div className="remove todos">
         <span className="todos__heading">Completed Tasks</span>
         {todos.map((todo) => (
           <SingleTodo
@@ -32,7 +34,7 @@ const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
             setTodos={setTodos}
           />
         ))}
-        </div>
+      </div>
     </div>
   );
 };
